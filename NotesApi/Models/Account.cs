30dc3d0f647b923de9aa9ce764eec5de77edfa35
Ali <ali512@gmail.com>
2023-08-23 13:03:34 +0300

    records

--- NotesApi/Models/Account.cs
+++ NotesApi/Models/Account.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 [Table("account")]
@@ -10,3 +11,26 @@ public class Account : Entity
     [Column("label")]
     public string Label { get; set; }
 }
+
+public record AccountQuery
+(
+    string? Label
+);
+
+public record AccountCreate
+(
+    [Required] Guid Id,
+    [Required] string Label
+);
+
+public record AccountUpdate
+(
+    [Required] Guid Id,
+    [Required] string Label
+);
+
+public record AccountModify
+(
+    Guid? Id,
+    string? Label
+);
